d14a76bc6099bfff35c17860611404216be7ec92
Nitish Raju <nitishraju@outlook.com>
2021-06-08 18:32:41 -0400

    Added util function to format dates

---
+++ src/utils/formatting.js
@@ -0,0 +1,29 @@
+/**
+ * Formats date object into the form MM/DD/YYYY.
+ * Passing 'true' as second argument collects and formats time as HH:MM.
+ */
+const formatDateObject = (dateObj, withTime=false) => {
+    const padZeros = (val) => {
+        return val <= 9 ? '0' + val : val.toString();
+    };
+
+    const year = dateObj.getFullYear();
+    const month = padZeros(dateObj.getMonth() + 1);
+    const date = padZeros(dateObj.getDate());
+    const dateString = month + '/' + date + '/' + year;
+
+    let timeString = '';
+    if (withTime) {
+        const hrs = padZeros(dateObj.getHours() % 12 || 12);
+        const mins = padZeros(dateObj.getMinutes());
+        const period = dateObj.getHours() >= 12 ? 'pm' : 'am';
+        timeString = ' ' + hrs + ':' + mins + period;
+    }
+
+    return dateString + timeString;
+};
+
+
+const utilFunctions = { formatDateObject };
+
+export default utilFunctions;
